9b1e113ed723c94583cee09124ff6ee2ecb18214
MiguelLouisLeRoux <miguellerouxleroux@gmail.com>
2021-04-23 16:40:52 +0200

    css

---
+++ registration_number.js
@@ -0,0 +1,23 @@
+let add = document.querySelector(".add");
+let clear = document.querySelector(".clear");
+let input = document.querySelector(".input");
+let output = document.querySelector(".number-plate");
+let errormess = document.querySelector(".error-message");
+let ca = document.querySelector(".ca");
+let cl = document.querySelector(".stel");
+let cj = document.querySelector(".pa");
+let caw = document.querySelector(".geo");
+
+const regFact = regFactFunc();
+
+function regNumber() {
+
+}
+
+add.addEventListener('click', regNumber);
+
+function clearReg() {
+
+}
+
+clear.addEventListener('click', clearReg);
