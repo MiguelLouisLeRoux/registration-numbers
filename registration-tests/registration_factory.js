447b2467145405b74a9e54d60c23dbb9fbff1153
MiguelLouisLeRoux <miguellerouxleroux@gmail.com>
2021-04-25 19:00:52 +0200

    completed css layout

--- registration-tests/registration_factory.js
+++ registration-tests/registration_factory.js
@@ -6,4 +6,36 @@ function regFactFunc() {
 
     //Registration number variable
     var regNum = "";
+
+    function setReg(input) {
+        regNum = input.trim(); 
+    }
+
+    function testRegNum(regNum){
+        if (regNum === "" || !/^[^a-zA-Z0-9]+$/.test(regNum)) {
+            error = noRegNum;
+            return error;
+        } else if (/^[^a-zA-Z0-9]+$/.test(regNum)) {
+
+        }
+    }
+
+    function noLoc() {
+        error = noFilt;
+        return error;
+        
+    }
+
+    function values() {
+        return {
+            theError : error,
+            theRegNum : regNum,
+        }
+    }
+
+    return { setReg,
+             testRegNum,
+             noLoc,
+             values
+    }
 }
--- registration_number.js
+++ registration_number.js
@@ -1,23 +1,29 @@
-let add = document.querySelector(".add");
-let clear = document.querySelector(".clear");
+let addBtn = document.querySelector(".add");
+let clearBtn = document.querySelector(".clear");
 let input = document.querySelector(".input");
 let output = document.querySelector(".number-plate");
+let list = document.getElementById("nList");
 let errormess = document.querySelector(".error-message");
 let ca = document.querySelector(".ca");
 let cl = document.querySelector(".stel");
 let cj = document.querySelector(".pa");
 let caw = document.querySelector(".geo");
 
-const regFact = regFactFunc();
+var regFact = regFactFunc();
 
 function regNumber() {
-
+    regFact.setReg(input.value);
+    var newDiv = document.createElement("div");
+    var newText = document.createTextNode(regFact.values().theRegNum);
+    newDiv.appendChild(newText);
+    newDiv.classList.add("number-plate");
+    // document.body.appendChild(newDiv);
 }
 
-add.addEventListener('click', regNumber);
+addBtn.addEventListener('click', regNumber);
 
 function clearReg() {
 
 }
 
-clear.addEventListener('click', clearReg);
+clearBtn.addEventListener('click', clearReg);
