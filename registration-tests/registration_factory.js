3e1798b0d4814bd2dc71c90eed496dd50e5d66d5
MiguelLouisLeRoux <miguellerouxleroux@gmail.com>
2021-04-26 16:47:05 +0200

    updatee

--- registration-tests/registration_factory.js
+++ registration-tests/registration_factory.js
@@ -7,18 +7,45 @@ function regFactFunc() {
     //Registration number variable
     var regNum = "";
 
+    //Regular expression
+    var reg1 = /([A-Z]){2}\s + ([0-9]){3}\s([0-9]){3}/g;
+    var reg2 = /([A-Z]){2}\- + ([0-9]){3}\-([0-9]){3}/g;
+    var reg3 = /([A-Z]){2}\s + ([0-9]){6}/g;
+    //Storage of registration numbers
+    var arr = [];
+
     function setReg(input) {
         regNum = input.trim(); 
     }
 
-    function testRegNum(regNum){
-        if (regNum === "" || !/^[^a-zA-Z0-9]+$/.test(regNum)) {
+    function testRegNum(radVal){
+        if (regNum === "" || !reg1.test(regNum)) {
             error = noRegNum;
+            regNum = "";
             return error;
-        } else if (/^[^a-zA-Z0-9]+$/.test(regNum)) {
+        } else if (reg1.test(regNum) || reg2.test(regNum) || reg3.test(regNum)) {
+            arr.push(regNum);
+            if (radVal === "capetown") {
+                for(var i = 0; i < arr.length; i++) {
+                    itt = arr[i];
+                    
+                    if (itt.startsWith("CA")) {
 
+                    }
+                }
+            } else if (radVal === "stellenbosch") {
+                
+            } else if (radVal === "paarl") {
+
+            } else if (radVal === "george") {
+
+            }
         }
     }
+
+    
+        
+    
 
     function noLoc() {
         error = noFilt;
@@ -33,9 +60,16 @@ function regFactFunc() {
         }
     }
 
+    function clear() {
+        regNum = "";
+        error = "";
+        localStorage.clear();
+    }
+
     return { setReg,
              testRegNum,
              noLoc,
-             values
+             values,
+             clear
     }
 }
--- registration_number.js
+++ registration_number.js
@@ -3,6 +3,7 @@ let clearBtn = document.querySelector(".clear");
 let input = document.querySelector(".input");
 let output = document.querySelector(".number-plate");
 let list = document.getElementById("nList");
+let theList = document.querySelector(".list-sect");
 let errormess = document.querySelector(".error-message");
 let ca = document.querySelector(".ca");
 let cl = document.querySelector(".stel");
@@ -12,18 +13,49 @@ let caw = document.querySelector(".geo");
 var regFact = regFactFunc();
 
 function regNumber() {
-    regFact.setReg(input.value);
+    var radio = document.querySelector("input[name='town']:checked");
     var newDiv = document.createElement("div");
     var newText = document.createTextNode(regFact.values().theRegNum);
     newDiv.appendChild(newText);
     newDiv.classList.add("number-plate");
-    // document.body.appendChild(newDiv);
+
+    if (!radio) {
+        regFact.setReg(input.value);
+        regFact.testRegNum();
+    } else if (radio) {
+        regFact.setReg(input.value);
+        regFact.testRegNum(radio.value);
+
+        
+        // theList.appendChild(newDiv);
+
+        if (radio.value === "capetown") {
+
+        } else if (radio.value === "stellenbosch") {
+
+        } else if (radio.value === "paarl") {
+
+        } else if (radio.value === "george") {
+
+        }
+    }
+    
+
+
+
+
+    
+
+    errormess.innerHTML = regFact.values().theError;
+    input.value = "";
 }
 
 addBtn.addEventListener('click', regNumber);
 
 function clearReg() {
-
+    regFact.clear();
+    
+    
 }
 
 clearBtn.addEventListener('click', clearReg);
